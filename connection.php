60edc19d4ffa9d9e7dcc8490e96ebf875bceac6f
Patrick Prakash <patrickprakash8@gmail.com>
2017-12-11 12:18:04 +0530

    Removed Connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$password='2w0rBN-}MR$b';
-$servername="localhost";
-$username="udigiupdater";
-$dbname="DIGI_BOARD_DB";
-mysql_connect("localhost","udigiupdater","$password") or die (mysql_error());
-mysql_select_db("DIGI_BOARD_DB")or die(mysql_error());
-$conn= new mysqli($servername, $username, $password, $dbname);
-?>
