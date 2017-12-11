e754610f63076a2eec42fa935ed16c5b6ed81680
Patrick Prakash <patrickprakash8@gmail.com>
2017-12-11 12:51:39 +0530

    Removed unwanted events

--- calendar/js/data.js
+++ calendar/js/data.js
@@ -1,13 +1,8 @@
 var events = {
-	'11-21-2016' : [{content: 'Great American Smokeout', url: 'http://www.wincalendar.com/Great-American-Smokeout', allDay: true}],
-	'11-11-2016' : [{content: 'Remembrance Day (Canada)', url: 'http://www.wincalendar.com/Remembrance-Day', allDay: true}], 
-	'04-01-2016' : [{content: 'Start Skagit Valley Tulip Festival ', repeat: 'YEARLY', allDay: true, endDate: '04-30-2100'}],
-	'12-25-2016' : [{content: 'Christmas Day', repeat: 'YEARLY', allDay: true, endDate: '12-25-2100'}],
+	'12-25-2016' : [{content: 'Christmas Day', repeat: 'YEARLY', allDay: true, endDate: '12-30-2017'}],
 	'01-01-2016' : [{content: 'New Year\'s', repeat: 'YEARLY', allDay: true, endDate: '12-31-2100'}],
-	'01-01-2016' : [{content: 'Yeah Monthly', repeat: 'MONTHLY', allDay: true, endDate: '07-02-2015'}],
-	'03-02-2016' : [{content : 'Graduation Exams', repeat: 'INTERVAL', allDay: true, endDate: '03-20-2015'}],
-	//'01-01-2016' : [{content : 'MONDAY (WEEKLY)', repeat: 'MON', allDay: true, endDate:'01-01-2100'}],
-    '12-05-2017' : [{content : 'Project Starts',  allDay: true,}]
+    '12-05-2017' : [{content : 'Project Starts',  allDay: true,}],
+    '12-11-2017' : [{content : 'Project Ends',  allDay: true,}]
 }
 //,
 //t = new Date(),
