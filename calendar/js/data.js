be6858c80e161ed41f2071f05c82573a5e62dc8c
Patrick Prakash <patrickprakash8@gmail.com>
2017-12-12 21:29:04 +0530

    Minor changes

--- calendar/js/data.js
+++ calendar/js/data.js
@@ -1,10 +1,10 @@
 var events = {
-	'12-25-2016' : [{content: 'Christmas Day', repeat: 'YEARLY', allDay: true, endDate: '12-30-2017'}],
-	'01-01-2016' : [{content: 'New Year\'s', repeat: 'YEARLY', allDay: true, endDate: '12-31-2100'}],
-    '12-05-2017' : [{content : 'Project Starts',  allDay: true,}],
-    '12-11-2017' : [{content : 'Project Ends',  allDay: true,}],
-    '12-12-2017' : [{content :  <?php echo json_encode($array[0]['REASON']); ?> allDay: true}] 
-}
+	                   '12-25-2016' : [{content: 'Christmas Day', repeat: 'YEARLY', allDay: true, endDate: '12-30-2017'}],
+	                   '01-01-2016' : [{content: 'New Year\'s', repeat: 'YEARLY', allDay: true, endDate: '12-31-2100'}],
+                       '12-05-2017' : [{content : 'Project Starts',  allDay: true,}],
+                       '12-11-2017' : [{content : 'Project Ends',  allDay: true,}],
+                       '12-12-2017' : [{content : '<?php echo array[0]['REASON'];?>',  allDay: true,}]
+             }
 //,
 //t = new Date(),
 //Creation of today event
